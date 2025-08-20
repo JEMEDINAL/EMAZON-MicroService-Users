46f7809a4bd73a11791fa7d7e33f818026c3c25f
JEMEDINAL <medinalondon2004@gmail.com>
2025-08-20 16:16:21 -0500

    correction of the error message regarding compliance with business rules

--- src/main/java/com/emazon/microservice_usuario/domain/constant/UserErrorMessage.java
+++ src/main/java/com/emazon/microservice_usuario/domain/constant/UserErrorMessage.java
@@ -4,15 +4,12 @@ public final class UserErrorMessage {
     public UserErrorMessage() {
 
     }
-    public static final String USER_NAME_CHARACTER = "The name must be between 2 and 150 characters long.";
-    public static final String USER_NOT_BLANK_NAME = "The name cannot be left blank.";
-    public static final String USER_LAST_NAME_CHARACTER = "The last name must be between 2 and 200 characters long.";
-    public static final String USER_NOT_BLANK_LAST_NAME = "The last name field cannot be left blank.";
+    public static final String USER_NAME_CHARACTER = "The name must be between 2 and 150 characters long and cannot be empty.";
+    public static final String USER_LAST_NAME_CHARACTER = "The last name must be between 2 and 200 characters long and cannot be left blank.";
     public static final String USER_PASSWORD_CHARACTER = "The password must be at least 8 characters long and cannot be empty.";
     public static final String USER_ID_NUMBER = "The ID number must be between 10 and 20.";
     public static final String USER_ID_NUMBER_NOT_BLANK = "The ID number cannot be empty.";
-    public static final String USER_PHONE_NUMBER_NOT_BLANK ="The Phone Number cannot be empty";
-    public static final String USER_PHONE_NUMBER = "The phone number cannot be longer than 14 characters.";
+    public static final String USER_PHONE_NUMBER = "The phone number cannot be longer than 14 characters and cannot be left blank.";
     public static final String USER_BIRTH_DATA_NOT_BLANK = "The Birth date cannot be empty";
     public static final String USER_EMAIL_NOT_BLANK ="The email date cannot be empty";
     public static final String USER_INVALID_EMAIL = "The email format is invalid required @ to .";
